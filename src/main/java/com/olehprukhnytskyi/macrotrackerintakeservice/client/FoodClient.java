0d48ebc8523e476229c3deba1dc5624a5261f481
Oleg Pruh <olegpruh1@gmail.com>
2025-05-26 20:07:02 +0100

    Fix: food service url corrected

--- src/main/java/com/olehprukhnytskyi/macrotrackerintakeservice/client/FoodClient.java
+++ src/main/java/com/olehprukhnytskyi/macrotrackerintakeservice/client/FoodClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "food-service", url = "${feign.goal-service:http://localhost:8081}")
+@FeignClient(name = "food-service", url = "${feign.food-service:http://localhost:8081}")
 public interface FoodClient {
     @GetMapping("/api/foods/{foodId}")
     FoodDto getFoodById(@PathVariable String foodId);
